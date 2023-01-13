4ef0a507229ddac73c22216e25fad3438e787743
nthmichelle <nathaniamichelle23@gmail.com>
2023-01-13 18:18:18 +0700

    add routing http

---
+++ src/4. routing http/routing.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	handlerIndex := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	}
+
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Again"))
+	})
+
+	fmt.Println("Sever started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
